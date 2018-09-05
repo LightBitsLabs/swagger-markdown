f931961f085ad73dcf85f872f29f381d6b7f9a98
Yogev Cohen <yogev@lightbitslabs.com>
2018-09-05 15:43:02 +0300

    add some new lines before headers for markdown linter.

--- app/transformers/info.js
+++ app/transformers/info.js
@@ -12,7 +12,8 @@ module.exports = info => {
   const res = [];
   if (info !== null && typeof info === 'object') {
     if ('title' in info) {
-      res.push(`# ${info.title}`);
+      res.push(`# ${info.title}\n`);
+      res.join('\n');
     }
 
     if ('description' in info) {
